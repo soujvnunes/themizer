44aaf2a28c57a922543ecc8967b1e8b5748cd3df
Victor <soujvnunes@gmail.com>
2024-10-18 22:31:44 +0100

    feat(types) add base interfaces

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type CSSCustomProperties = {
+  [key: string]: string | number;
+};
+
+export interface Vars<Aliases extends object> {
+  value: CSSCustomProperties;
+  aliases: Aliases;
+}
+
+export interface ThemeOptions<Tokens extends object = object> {
+  tokens: Tokens;
+  keysPrefix?: string;
+}

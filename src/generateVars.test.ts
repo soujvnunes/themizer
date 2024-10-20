67721b57c854df2edcd95e1caad3f6ff15b44d21
Victor <soujvnunes@gmail.com>
2024-10-20 20:32:13 +0100

    refactor(generateVars) adapt to accept options.medias

--- src/generateVars.test.ts
+++ src/generateVars.test.ts
@@ -18,7 +18,7 @@ it('should generate vars correctly', () => {
     '--colors-red-500': '#f00',
     '--spaces-md': '1rem',
   });
-  expect(vars.aliases).toMatchObject({
+  expect(vars.reference).toMatchObject({
     colors: {
       white: 'var(--colors-white, #fff)',
       red: {
@@ -45,7 +45,7 @@ it('should prefix generated vars correctly', () => {
       },
     },
     {
-      prefixKeys: 'tokens',
+      prefixProperties: 'tokens',
     },
   );
 
@@ -54,7 +54,7 @@ it('should prefix generated vars correctly', () => {
     '--tokens-colors-red-500': '#f00',
     '--tokens-spaces-md': '1rem',
   });
-  expect(vars.aliases).toMatchObject({
+  expect(vars.reference).toMatchObject({
     colors: {
       white: 'var(--tokens-colors-white, #fff)',
       red: {
@@ -63,6 +63,102 @@ it('should prefix generated vars correctly', () => {
     },
     spaces: {
       md: 'var(--tokens-spaces-md, 1rem)',
+    },
+  });
+});
+
+it("should replace correspondent responsive vars' media queries correctly", () => {
+  const vars = generateVars(
+    {
+      colors: {
+        black: '#111',
+        gray: [{ print: '#fff' }, '#ddd'],
+        white: [{ dark: '#333' }, '#fff'],
+        red: {
+          500: [{ print: '#000' }, '#f00'],
+        },
+      },
+      spaces: {
+        md: [{ desktop: '1.5rem' }, '1rem'],
+      },
+      font: {
+        sans: 'sofia-pro',
+        sizes: {
+          xl: [{ desktop: '3rem' }, '2rem'],
+        },
+      },
+      alphas: {
+        primary: [{ dark: 1 }, 0.8],
+        secondary: 0.6,
+      },
+      trans: {
+        bounce: [{ motion: '200ms cubic-bezier(0.5, -0.5, 0.25, 1.5)' }],
+        ease: [{ motion: '200ms cubic-bezier(0.25, 0.1, 0.25, 1)' }],
+      },
+    },
+    {
+      medias: {
+        desktop: '@media (min-width: 1024px)',
+        dark: '@media (prefers-color-scheme: dark)',
+        motion: '@media (prefers-reduced-motion: no-preference)',
+        print: '@media print',
+      },
+    } as const,
+  );
+
+  expect(vars.value).toMatchObject({
+    '--colors-black': '#111',
+    '--colors-gray': '#ddd',
+    '--colors-white': '#fff',
+    '--colors-red-500': '#f00',
+    '--spaces-md': '1rem',
+    '--font-sizes-xl': '2rem',
+    '--font-sans': 'sofia-pro',
+    '--alphas-primary': 0.8,
+    '--alphas-secondary': 0.6,
+    '@media (prefers-color-scheme: dark)': {
+      '--colors-white': '#333',
+      '--alphas-primary': 1,
+    },
+    '@media print': {
+      '--colors-gray': '#fff',
+      '--colors-red-500': '#000',
+    },
+    '@media (min-width: 1024px)': {
+      '--spaces-md': '1.5rem',
+      '--font-sizes-xl': '3rem',
+    },
+    '@media (prefers-reduced-motion: no-preference)': {
+      '--trans-bounce': '200ms cubic-bezier(0.5, -0.5, 0.25, 1.5)',
+      '--trans-ease': '200ms cubic-bezier(0.25, 0.1, 0.25, 1)',
+    },
+  });
+  expect(vars.reference).toMatchObject({
+    colors: {
+      black: 'var(--colors-black, #111)',
+      gray: 'var(--colors-gray, #ddd)',
+      white: 'var(--colors-white, #fff)',
+      red: {
+        500: 'var(--colors-red-500, #f00)',
+      },
+    },
+    spaces: {
+      md: 'var(--spaces-md, 1rem)',
+    },
+    font: {
+      sans: 'var(--font-sans, sofia-pro)',
+      sizes: {
+        xl: 'var(--font-sizes-xl, 2rem)',
+      },
+    },
+    alphas: {
+      primary: 'var(--alphas-primary, 0.8)',
+      secondary: 'var(--alphas-secondary, 0.6)',
+    },
+    // No default value, because it's only value within `@media`
+    trans: {
+      bounce: 'var(--trans-bounce)',
+      ease: 'var(--trans-ease)',
     },
   });
 });
--- src/generateVars.ts
+++ src/generateVars.ts
@@ -1,51 +1,95 @@
-import type { ThemeOptions, Vars } from './types';
+import isPrimitive from './isPrimitive';
+import type { GenerateVarsOptions, Schema, Vars } from './types';
 
-export default function generateVars<Aliases extends object>(
-  aliases: Aliases,
-  options?: Omit<ThemeOptions, 'tokens'>,
+const PROPERTIES_UNIFIER = '-';
+
+let cssCustomProperties = {};
+
+export default function generateVars<
+  M extends string = never,
+  S extends Schema<M> = Schema<M>,
+>(
+  schema: S,
+  options?: GenerateVarsOptions<M>,
   __adjustor?: string,
-): Vars<Aliases> {
-  return Object.entries(aliases).reduce((acc, [key, value]) => {
-    const keysUnifier = '-';
-    const prefixKeys = options?.prefixKeys
-      ? `${options.prefixKeys}${keysUnifier}`
-      : '';
-    const adjustor = __adjustor ? `${__adjustor}${keysUnifier}` : '';
-    const path = `${prefixKeys}${adjustor}${key}`;
+): Vars<S, M> {
+  const prefixProperties = options?.prefixProperties
+    ? `${options.prefixProperties}${PROPERTIES_UNIFIER}`
+    : '';
+  const adjustor = __adjustor ? `${__adjustor}${PROPERTIES_UNIFIER}` : '';
+
+  return Object.entries(schema).reduce((generatedVars, [property, value]) => {
+    const path = `${prefixProperties}${adjustor}${property}`;
     const finalPath = `--${path}`;
 
-    if (typeof value === 'object' && value != null) {
-      const vars = generateVars(
-        value,
-        {
-          prefixKeys: '',
-        },
-        path,
-      );
-
+    if (isPrimitive(value)) {
       return {
-        ...acc,
+        ...generatedVars,
         value: {
-          ...acc.value,
-          ...vars.value,
+          ...generatedVars.value,
+          [finalPath]: value,
         },
-        aliases: {
-          ...acc.aliases,
-          [key]: vars.aliases,
+        reference: {
+          ...generatedVars.reference,
+          [property]: `var(${finalPath}, ${value})`,
         },
       };
     }
 
-    return {
-      ...acc,
-      value: {
-        ...acc.value,
-        [finalPath]: value,
+    if (Array.isArray(value)) {
+      const [medias, defaultValue] = value;
+      const hasDefaultValue = isPrimitive(defaultValue);
+      const resolvedDefaultValue = hasDefaultValue ? `, ${defaultValue}` : '';
+
+      // TODO: prefer using reduce and send it through another private parameter
+      for (const [media, val] of Object.entries(medias)) {
+        const mediaQuery = options?.medias?.[media as M] as string;
+
+        cssCustomProperties = {
+          ...cssCustomProperties,
+          [mediaQuery]: {
+            // @ts-expect-error It's always an object
+            ...(cssCustomProperties as CSSCustomProperties<M>)[mediaQuery],
+            [finalPath]: val,
+          },
+        };
+      }
+
+      return {
+        ...generatedVars,
+        value: {
+          ...generatedVars.value,
+          ...(hasDefaultValue && {
+            [finalPath]: defaultValue,
+          }),
+          ...cssCustomProperties,
+        },
+        reference: {
+          ...generatedVars.reference,
+          [property]: `var(${finalPath}${resolvedDefaultValue})`,
+        },
+      };
+    }
+
+    const vars = generateVars(
+      value,
+      {
+        prefixProperties: '',
+        medias: options?.medias,
       },
-      aliases: {
-        ...acc.aliases,
-        [key]: `var(${finalPath}, ${value})`,
+      path,
+    );
+
+    return {
+      ...generatedVars,
+      value: {
+        ...generatedVars.value,
+        ...vars.value,
+      },
+      reference: {
+        ...generatedVars.reference,
+        [property]: vars.reference,
       },
     };
-  }, {} as Vars<Aliases>);
+  }, {} as Vars<S, M>);
 }

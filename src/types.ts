43b56f7e50e9c6e327c6e09f48052629a77c05aa
Victor <soujvnunes@gmail.com>
2024-10-21 19:58:10 +0100

    feat(types) enhance naming

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,22 @@
-export type Medias<M extends string = never> = {
+export type ResponsiveSchema<M extends string> = {
   [Media in M]: string | number;
 };
 
 export interface Schema<M extends string = never> {
   [prop: string]:
     | (string | number | Schema<M>)
-    | (M extends string ? [Medias<M>, (string | number)?] : never);
+    | (M extends string ? [ResponsiveSchema<M>, (string | number)?] : never);
 }
 
-type PurifySchema<S extends Schema<string>, M extends string = never> = {
+export type PurifySchema<M extends string, S extends Schema<M>> = {
   [Prop in keyof S]: S[Prop] extends [infer Media, infer Default]
     ? Default extends string | number
       ? Default
-      : Media extends Medias<M>
-      ? Medias<M>[M]
+      : Media extends ResponsiveSchema<M>
+      ? ResponsiveSchema<M>[M]
       : never
     : S[Prop] extends Schema<M>
-    ? PurifySchema<S[Prop], M>
+    ? PurifySchema<M, S[Prop]>
     : S[Prop];
 };
 
@@ -37,11 +37,10 @@ export interface GenerateVarsOptions<M extends string = never> {
 
 export interface GeneratedVars<S extends Schema<M>, M extends string = never> {
   value: Vars & M extends string ? ResponsiveVars : never;
-  // TODO: Remove Medias and strings (V extends [infer D, any] ? D extends string | number ? D : D extends object ? ObjectValue<D> : never : never )
-  reference: PurifySchema<S, M>;
+  reference: PurifySchema<M, S>;
 }
 
 export interface ThemeOptions<M extends string, T extends Schema>
   extends GenerateVarsOptions<M> {
-  tokens: T;
+  tokens?: T;
 }

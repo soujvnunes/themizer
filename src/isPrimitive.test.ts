9c4db314cd40604ef20c66f09af7feeae779ea92
Victor <soujvnunes@gmail.com>
2024-10-25 20:50:19 +0100

    test(isPrimitive) adjust

--- src/isPrimitive.test.ts
+++ src/isPrimitive.test.ts
@@ -1,8 +1,32 @@
 import isPrimitive from './isPrimitive';
 
-it('should return boolean based on the parameter correctly', () => {
-  expect(isPrimitive(0)).toBeTruthy();
-  expect(isPrimitive('a')).toBeTruthy();
-  expect(isPrimitive([])).toBeFalsy();
-  expect(isPrimitive({})).toBeFalsy();
+describe('isPrimitive', () => {
+  describe('when providing a number parameter', () => {
+    it('returns true', () => {
+      expect(isPrimitive(0)).toBeTruthy();
+    });
+  });
+  describe('when providing a string parameter', () => {
+    it('returns true', () => {
+      expect(isPrimitive('a')).toBeTruthy();
+    });
+  });
+  describe('when providing an object parameter', () => {
+    describe('array-like', () => {
+      it('returns false', () => {
+        expect(isPrimitive([])).toBeFalsy();
+      });
+    });
+    describe('object-like', () => {
+      it('returns false', () => {
+        expect(isPrimitive({})).toBeFalsy();
+      });
+    });
+  });
+  describe('when providing null or undefined parameter', () => {
+    it('returns false', () => {
+      expect(isPrimitive(null)).toBeFalsy();
+      expect(isPrimitive(undefined)).toBeFalsy();
+    });
+  });
 });

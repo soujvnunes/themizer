005d65ef663180c0ee6eabcf1ac63426f268b5cb
Victor <soujvnunes@gmail.com>
2024-10-28 12:35:07 +0000

    feat(getTheme) adjust tokens aliases' parameter it's not responsive

--- src/getTheme.test.ts
+++ src/getTheme.test.ts
@@ -1,47 +1,34 @@
-import dispatchVars from './dispatchVars';
-import getRules from './getRules';
 import getTheme from './getTheme';
-
-jest.mock('./getRules');
-jest.mock('./dispatchVars');
-
-afterAll(() => {
-  jest.resetAllMocks();
-});
 
 describe('getTheme', () => {
   describe('taking aliases', () => {
     const theme = getTheme({
       font: {
         sizes: {
-          md: 16,
+          md: '16px',
         },
       },
     });
 
     it('returns its reference', () => {
-      expect(theme.aliases).toEqual({
-        font: {
-          sizes: {
-            md: 'var(--aliases-font-sizes-md, 16)',
+      expect(theme).toEqual({
+        aliases: {
+          font: {
+            sizes: {
+              md: 'var(--aliases-font-sizes-md, 16px)',
+            },
           },
         },
+        rules:
+          '@layer theme;@layer theme{:root{--aliases-font-sizes-md:16px;}}',
       });
-      expect(theme.medias).toBeUndefined();
-      expect(theme.tokens).toBeUndefined();
-    });
-    it('dispatches stringified vars', () => {
-      expect(getRules).toHaveBeenCalledWith({ '--aliases-font-sizes-md': 16 });
-      expect(dispatchVars).toHaveBeenCalled();
-      expect(dispatchVars).toHaveReturned();
-      //'@layer theme;@layer theme{:root{--aliases-font-sizes-md: 16}}',
     });
   });
   describe('taking aliases with options', () => {
     const theme = getTheme(
       (tokens) => ({
         palette: {
-          main: [{ dark: tokens.colors.amber[400] }, tokens.colors.amber[600]],
+          main: [{ dark: tokens.colors.amber.light }, tokens.colors.amber.dark],
         },
       }),
       {
@@ -52,50 +39,35 @@ describe('getTheme', () => {
         tokens: {
           colors: {
             amber: {
-              400: '#fbbf24',
-              600: '#d97706',
+              light: '#fbbf24',
+              dark: '#d97706',
             },
           },
         },
       },
     );
 
-    it('returns its reference prefixed', () => {
-      expect(theme.aliases).toEqual({
-        palette: {
-          main: 'var(--ds-aliases-palette-main, var(--ds-tokens-colors-amber-600, #d97706))',
-        },
-      });
-    });
-    it('returns its media reference', () => {
-      expect(theme.medias).toEqual({
-        dark: '@media (prefers-scheme-color: dark)',
-      });
-    });
-    it('returns its tokens reference prefixed', () => {
-      expect(theme.tokens).toEqual({
-        colors: {
-          amber: {
-            '400': 'var(--ds-tokens-colors-amber-400, #fbbf24)',
-            '600': 'var(--ds-tokens-colors-amber-600, #d97706)',
+    it('returns its reference and tokens prefixed with medias one', () => {
+      expect(theme).toEqual({
+        aliases: {
+          palette: {
+            main: 'var(--ds-aliases-palette-main, var(--ds-tokens-colors-amber-dark, #d97706))',
           },
         },
-      });
-    });
-    it('dispatches stringified vars', () => {
-      expect(getRules).toHaveBeenCalledWith({
-        '--ds-aliases-palette-main':
-          'var(--ds-tokens-colors-amber-600, #d97706)',
-        '--ds-tokens-colors-amber-400': '#fbbf24',
-        '--ds-tokens-colors-amber-600': '#d97706',
-        '@media (prefers-scheme-color: dark)': {
-          '--ds-aliases-palette-main':
-            'var(--ds-tokens-colors-amber-400, #fbbf24)',
+        medias: {
+          dark: '@media (prefers-scheme-color: dark)',
         },
+        tokens: {
+          colors: {
+            amber: {
+              light: 'var(--ds-tokens-colors-amber-light, #fbbf24)',
+              dark: 'var(--ds-tokens-colors-amber-dark, #d97706)',
+            },
+          },
+        },
+        rules:
+          '@layer theme;@layer theme{:root{--ds-tokens-colors-amber-light:#fbbf24;--ds-tokens-colors-amber-dark:#d97706;--ds-aliases-palette-main:var(--ds-tokens-colors-amber-dark, #d97706);@media (prefers-scheme-color: dark){--ds-aliases-palette-main:var(--ds-tokens-colors-amber-light, #fbbf24);}}}',
       });
-      expect(dispatchVars).toHaveBeenCalled();
-      expect(dispatchVars).toHaveReturned();
-      //'@layer theme;@layer theme{:root{--aliases-font-sizes-md: 16}}',
     });
   });
 });
--- src/getTheme.ts
+++ src/getTheme.ts
@@ -1,33 +1,25 @@
-import type {
-  GeneratedVars,
-  PurifySchema,
-  Schema,
-  ThemeOptions,
-} from './types';
+import type { GeneratedVars, Schema, ThemeOptions } from './types';
 import generateVars from './generateVars';
 import resolveThemePrefix from './resolveThemePrefix';
-import dispatchVars from './dispatchVars';
 import getRules from './getRules';
 
 export default function getTheme<
   M extends string,
   T extends Schema,
-  A extends Schema<M> = Schema<M>,
-  R extends PurifySchema<M, T> = PurifySchema<M, T>,
+  A extends Schema<M>,
   // TODO: create overloads to handle scenaries in which there's no tokens to be passed by aliases parameter
->(aliases: A | ((tokensReference: R) => A), options?: ThemeOptions<M, T>) {
-  let currentRules = '';
-  let tokensVars: GeneratedVars<M, T> | undefined;
+>(aliases: A | ((tokens: T) => A), options?: ThemeOptions<M, T>) {
+  let tokensVars: GeneratedVars<never, T> | undefined;
 
   if (typeof options?.tokens !== 'undefined') {
-    tokensVars = generateVars<M, T>(options.tokens, {
+    tokensVars = generateVars(options.tokens, {
       prefixProperties: resolveThemePrefix('tokens', options?.prefixProperties),
     });
   }
 
   const resolvedAliases =
     typeof aliases === 'function'
-      ? aliases(tokensVars?.reference as R)
+      ? aliases(tokensVars?.reference as T)
       : aliases;
   const aliasesVars = generateVars<M, A>(resolvedAliases, {
     ...options,
@@ -38,11 +30,9 @@ export default function getTheme<
     ...aliasesVars.value,
   });
 
-  if (rules !== currentRules) {
-    currentRules = dispatchVars(rules);
-  }
-
   return {
+    // TODO: use wyw-in-js to process those rules into a css file
+    rules,
     aliases: aliasesVars.reference,
     tokens: tokensVars?.reference,
     medias: options?.medias,

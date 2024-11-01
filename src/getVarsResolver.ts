cf2dafff80684917790e71400036335b290eec1c
Victor <soujvnunes@gmail.com>
2024-11-01 20:25:32 +0000

    feat(getVarsResolver) adjust reference type

--- src/getVarsResolver.ts
+++ src/getVarsResolver.ts
@@ -1,18 +1,20 @@
-import type { GeneratedVars, Primitive, Schema, Vars } from './types';
+import type {
+  GeneratedVars,
+  Primitive,
+  ResolveSchema,
+  Schema,
+  Vars,
+} from './types';
 
-export default function getVarsResolver<
-  M extends string = never,
-  S extends Schema<M> = Schema<M>,
->(generatedVars: GeneratedVars<M, S>) {
-  return (value: Record<string, Vars | Primitive>, reference: Schema<M>) => ({
+export default function getVarsResolver<M extends string, S extends Schema<M>>(
+  generatedVars: GeneratedVars<M, S>,
+) {
+  return (
+    value: Record<string, Vars | Primitive>,
+    reference: Record<string, ResolveSchema<M, Schema<M>> | string>,
+  ) => ({
     ...generatedVars,
-    value: {
-      ...generatedVars.value,
-      ...value,
-    },
-    reference: {
-      ...generatedVars.reference,
-      ...reference,
-    },
+    value: { ...generatedVars.value, ...value },
+    reference: { ...generatedVars.reference, ...reference },
   });
 }

846d91308fe7c5fc08411919a7789c9743111868
Victor <soujvnunes@gmail.com>
2024-11-04 20:24:56 +0000

    test(getTheme) responsive values

--- src/getTheme.test.ts
+++ src/getTheme.test.ts
@@ -1,4 +1,7 @@
+import 'jest-puppeteer';
+import 'expect-puppeteer';
 import getTheme from './getTheme';
+import resolveVar from './resolveVar';
 
 describe('getTheme', () => {
   describe('taking aliases and options', () => {
@@ -7,20 +10,23 @@ describe('getTheme', () => {
         palette: {
           main: [{ dark: tokens.colors.amber.light }, tokens.colors.amber.dark],
         },
+        spacing: { md: tokens.units[24] },
+        sizing: {
+          md: [{ desktop: tokens.units[24] }, tokens.units[16]],
+        },
       }),
       {
         prefixVars: 'ds',
         medias: {
           dark: '@media (prefers-scheme-color: dark)',
+          desktop: '@media (min-width: 1024px)',
         },
         tokens: {
           colors: {
-            amber: {
-              light: '#fbbf24',
-              dark: '#d97706',
-            },
+            amber: { light: 'rgb(251, 191, 36)', dark: 'rgb(217, 119, 6)' },
           },
-        },
+          units: { '24': '24px', '16': '16px' },
+        } as const,
       },
     );
 
@@ -28,94 +34,73 @@ describe('getTheme', () => {
       expect(theme).toEqual({
         aliases: {
           palette: {
-            main: 'var(--ds-aliases-palette-main, var(--ds-tokens-colors-amber-dark, #d97706))',
+            main: 'var(--ds-aliases-palette-main, var(--ds-tokens-colors-amber-dark, rgb(217, 119, 6)))',
+          },
+          sizing: {
+            md: 'var(--ds-aliases-sizing-md, var(--ds-tokens-units-16, 16px))',
+          },
+          spacing: {
+            md: 'var(--ds-aliases-spacing-md, var(--ds-tokens-units-24, 24px))',
           },
         },
         medias: {
           dark: '@media (prefers-scheme-color: dark)',
+          desktop: '@media (min-width: 1024px)',
         },
         tokens: {
           colors: {
             amber: {
-              light: 'var(--ds-tokens-colors-amber-light, #fbbf24)',
-              dark: 'var(--ds-tokens-colors-amber-dark, #d97706)',
+              light: 'var(--ds-tokens-colors-amber-light, rgb(251, 191, 36))',
+              dark: 'var(--ds-tokens-colors-amber-dark, rgb(217, 119, 6))',
             },
+          },
+          units: {
+            '16': 'var(--ds-tokens-units-16, 16px)',
+            '24': 'var(--ds-tokens-units-24, 24px)',
           },
         },
         rules:
-          '@layer theme;@layer theme{:root{--ds-tokens-colors-amber-light:#fbbf24;--ds-tokens-colors-amber-dark:#d97706;--ds-aliases-palette-main:var(--ds-tokens-colors-amber-dark, #d97706);@media (prefers-scheme-color: dark){--ds-aliases-palette-main:var(--ds-tokens-colors-amber-light, #fbbf24);}}}',
+          '@layer theme;@layer theme{:root{--ds-tokens-colors-amber-light:rgb(251, 191, 36);--ds-tokens-colors-amber-dark:rgb(217, 119, 6);--ds-tokens-units-16:16px;--ds-tokens-units-24:24px;--ds-aliases-palette-main:var(--ds-tokens-colors-amber-dark, rgb(217, 119, 6));@media (prefers-scheme-color: dark){--ds-aliases-palette-main:var(--ds-tokens-colors-amber-light, rgb(251, 191, 36));}--ds-aliases-spacing-md:var(--ds-tokens-units-24, 24px);--ds-aliases-sizing-md:var(--ds-tokens-units-16, 16px);@media (min-width: 1024px){--ds-aliases-sizing-md:var(--ds-tokens-units-24, 24px);}}}',
       });
     });
+    it('its styles are applied to the DOM', async () => {
+      await page.setContent(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <style>${theme.rules}</style> 
+          </head>
+          <body>
+            <p id="element" style="color: ${theme.aliases.palette.main}; font-size: ${theme.aliases.sizing.md}"></p> 
+          </body>
+        </html>
+      `);
 
-    describe('with multiple medias', () => {
-      const theme = getTheme(
-        (tokens) => ({
-          palette: {
-            main: [
-              { dark: tokens.colors.amber.light },
-              tokens.colors.amber.dark,
-            ],
-          },
-          spacing: {
-            md: tokens.units[24],
-          },
-          sizing: {
-            md: [{ desktop: tokens.units[24] }, tokens.units[16]],
-          },
-        }),
-        {
-          prefixVars: 'ds',
-          medias: {
-            dark: '@media (prefers-scheme-color: dark)',
-            desktop: '@media (min-width: 1024px)',
-          },
-          tokens: {
-            colors: {
-              amber: {
-                light: '#fbbf24',
-                dark: '#d97706',
-              },
-            },
-            units: {
-              '24': '1.5rem',
-              '16': '1rem',
-            },
-          } as const,
-        },
-      );
+      function getComputedStyle() {
+        return page.evaluate(() => {
+          const element = document.getElementById('element');
 
-      it('returns each CSS rule within related media', () => {
-        expect(theme).toEqual({
-          aliases: {
-            palette: {
-              main: 'var(--ds-aliases-palette-main, var(--ds-tokens-colors-amber-dark, #d97706))',
-            },
-            sizing: {
-              md: 'var(--ds-aliases-sizing-md, var(--ds-tokens-units-16, 1rem))',
-            },
-            spacing: {
-              md: 'var(--ds-aliases-spacing-md, var(--ds-tokens-units-24, 1.5rem))',
-            },
-          },
-          medias: {
-            dark: '@media (prefers-scheme-color: dark)',
-            desktop: '@media (min-width: 1024px)',
-          },
-          tokens: {
-            colors: {
-              amber: {
-                light: 'var(--ds-tokens-colors-amber-light, #fbbf24)',
-                dark: 'var(--ds-tokens-colors-amber-dark, #d97706)',
-              },
-            },
-            units: {
-              '16': 'var(--ds-tokens-units-16, 1rem)',
-              '24': 'var(--ds-tokens-units-24, 1.5rem)',
-            },
-          },
-          rules:
-            '@layer theme;@layer theme{:root{--ds-tokens-colors-amber-light:#fbbf24;--ds-tokens-colors-amber-dark:#d97706;--ds-tokens-units-16:1rem;--ds-tokens-units-24:1.5rem;--ds-aliases-palette-main:var(--ds-tokens-colors-amber-dark, #d97706);@media (prefers-scheme-color: dark){--ds-aliases-palette-main:var(--ds-tokens-colors-amber-light, #fbbf24);}--ds-aliases-spacing-md:var(--ds-tokens-units-24, 1.5rem);--ds-aliases-sizing-md:var(--ds-tokens-units-16, 1rem);@media (min-width: 1024px){--ds-aliases-sizing-md:var(--ds-tokens-units-24, 1.5rem);}}}',
+          if (!element) return;
+
+          const { color, fontSize } = window.getComputedStyle(element);
+
+          return { color, fontSize };
         });
+      }
+
+      expect(await getComputedStyle()).toEqual({
+        color: resolveVar(theme.tokens.colors.amber.dark),
+        fontSize: resolveVar(theme.tokens.units[16]),
+      });
+
+      await page.emulateMediaFeatures([
+        { name: 'prefers-color-scheme', value: 'dark' },
+      ]);
+      await page.setViewport({ width: 1080, height: 1024 });
+
+      expect(await getComputedStyle()).toEqual({
+        color: resolveVar(theme.tokens.colors.amber.dark),
+        fontSize: resolveVar(theme.tokens.units[24]),
       });
     });
   });

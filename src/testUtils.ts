0cdd670f8b129a8a6d45a0685a6dfc3e8fff0c35
Victor <soujvnunes@gmail.com>
2024-11-06 17:55:21 +0000

    feat(testUtils) return styles after setting screen type

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,22 +1,13 @@
 import 'jest-puppeteer';
 import 'expect-puppeteer';
 
-export async function renderUI(html: string) {
+export async function render(html: string) {
   await page.setContent(html);
 
   return {
-    getStyle: () => {
-      return page.evaluate(() => {
-        const element = document.getElementById('element');
-
-        if (!element) return;
-
-        const { color, fontSize } = window.getComputedStyle(element);
-
-        return { color, fontSize };
-      });
-    },
-    setType: async (type: `${'mobile' | 'desktop'}.${'dark' | 'light'}`) => {
+    setScreenType: async (
+      type: `${'mobile' | 'desktop'}.${'dark' | 'light'}`,
+    ) => {
       await page.emulateMediaFeatures([
         {
           name: 'prefers-color-scheme',
@@ -26,6 +17,16 @@ export async function renderUI(html: string) {
       await page.setViewport({
         width: /mobile/.test(type) ? 540 : 1080,
         height: 1024,
+      });
+
+      return page.evaluate(() => {
+        const element = document.getElementById('element');
+
+        if (!element) return;
+
+        const { color, fontSize } = window.getComputedStyle(element);
+
+        return { color, fontSize };
       });
     },
   };

21694dd88b5ccca92bf18cc00bd03be4b8633c8d
Victor <soujvnunes@gmail.com>
2025-05-05 10:16:54 -0300

    test(test-utils/renderHTML) add style tag options

--- src/test-utils/renderHTML.ts
+++ src/test-utils/renderHTML.ts
@@ -1,10 +1,18 @@
 import 'jest-puppeteer'
 import 'expect-puppeteer'
+import { type FrameAddStyleTagOptions } from 'puppeteer'
 
-type ScreenType = `${'mobile' | 'desktop'}.${'dark' | 'light'}`
+type ScreenViewportAliases = 'mobile' | 'desktop'
+type ScreenColorSchemes = 'dark' | 'light'
+type ScreenType = `${ScreenViewportAliases}.${ScreenColorSchemes}`
 
-export default async function renderHTML(html: string) {
-  await page.setContent(html)
+export default async function renderHTML(
+  content: string,
+  options?: Omit<FrameAddStyleTagOptions, 'url'>,
+) {
+  await page.setContent(content)
+
+  if (options) await page.addStyleTag(options)
 
   return {
     async setScreenType(type: ScreenType) {
